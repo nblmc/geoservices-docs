8a6c89f7ec983d8239af8ba3cbf63cef890cc8fa
Victoria Mak <33503617+viymak@users.noreply.github.com>
2020-02-14 15:25:50 -0500

    Update code-modules-backup.py

--- workshops/media/code-modules-backup.py
+++ workshops/media/code-modules-backup.py
@@ -8,16 +8,6 @@ bpl_branches = pd.DataFrame(libraries)
 # Drop Columns
 # Syntax: your_dataframe.drop('1st_dropped_column','2nd_column', 'n_dropped_column' ) - Use axis 0 for rows and axis 1 for columns
 drop_columns = bpl_branches.drop(columns=['phone', 'network'])
-print(drop_columns)#remove example
-import pandas as pd
-
-#create dataframe
-libraries = pd.read_csv('libraries.csv')
-bpl_branches = pd.DataFrame(libraries)
-
-# Drop Columns
-# Syntax: your_dataframe.drop('1st_dropped_column','2nd_column', 'n_dropped_column' ) - Use axis 0 for rows and axis 1 for columns
-drop_columns = bpl_branches.drop(columns=['phone', 'network'])
 print(drop_columns)
 
 #### ** Tutorial **
@@ -45,3 +35,46 @@ drop_na = bpl_branches.dropna(subset=['address'])
 dropna.head()
 ```
 <!-- tabs:end -->
+
+
+filter
+
+exampleimport pandas as pd
+
+#create dataframe
+data = pd.read_csv('grades.csv')
+grades = pd.DataFrame(data)
+
+# filter data to only show exams
+# syntax: dataframe[dataframe['column'].str.contains('string we are looking for')]
+# adding " ~ " as in the example below will reverse the function and return rows that DO NOT CONTAIN
+
+assessments = grades[~ grades['assignment_type'].str.contains('hw', na=False)]
+
+#check results
+print(assessments)
+
+data example 
+student,assignment_type,grade_%
+Arnaud,exam,
+Arnaud,hw,92
+Arnaud,quiz 1,82
+Binti,exam,72
+Binti,hw,83
+Binti,quiz 1,82
+Cyrus,exam,94
+Cyrus,hw,82
+Cyrus,exam,81
+
+filter_tutorial
+import pandas as pd
+
+#create dataframe
+data = pd.read_csv('grades.csv')
+grades = pd.DataFrame(data)
+
+# filter data to only show exams
+exams = grades[grades['assignment_type'].str.contains('exam', na=False)]
+
+#check results
+print(exams)
